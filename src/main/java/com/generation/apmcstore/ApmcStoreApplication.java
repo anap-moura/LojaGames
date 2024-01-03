15cc4563bff83f4be4d4c7748f2841d90914da3c
Ana Paula Moura <ana.mcruz@hotmail.com>
2024-01-03 19:27:27 -0300

    primeiro commit

---
+++ src/main/java/com/generation/apmcstore/ApmcStoreApplication.java
@@ -0,0 +1,14 @@
+package com.generation.apmcstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@SpringBootApplication @EnableSpringDataWebSupport 
+public class ApmcStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmcStoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/apmcstore/model/Categoria.java
@@ -0,0 +1,54 @@
+package com.generation.apmcstore.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table (name = "tb_categoria")
+
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank (message = "A categoria precisa ter um nome!")
+	@Size(min = 5, max = 50, message = "O nome da categoria deve ter no mínimo 5 caracteres")
+	@Column(length = 50)
+	private String tipo;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> produto;
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
